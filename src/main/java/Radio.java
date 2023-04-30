08efb7a740d939f0fad6d0ad62f86265542bf185
Alexey 1981 <maximenko.av@rambler.ru>
2023-04-30 14:44:57 +0300

    Initial commit

--- src/main/java/Radio.java
+++ src/main/java/Radio.java
@@ -1,64 +1,17 @@
 public class Radio {
+//    public int numberFirst=20;
+//    public int getNumberSecond=10;
 
-
-    private int numberRadio;
-    private int valueRadio;
-
-    public void setNextChannel(int newNumberRadio) {
-        numberRadio = newNumberRadio;
-
-        if (newNumberRadio > 9) {
-            numberRadio = 9;
+    public void changeNumber(int numberFirst, int getNumberSecond){
+        if (numberFirst>getNumberSecond){
+            System.out.println("Все норм");
         }
-        if (newNumberRadio < 0) {
-            numberRadio = 0;
-        }
-
-    }
-
-    public int getNumberRadio() {
-
-        return numberRadio;
-    }
-
-    public void next() {
-        if (numberRadio < 9) {
-            numberRadio += 1;
-        } else numberRadio = 0;
-    }
-
-    public void prev() {
-        if (numberRadio > 0) {
-            numberRadio -= 1;
-        } else numberRadio = 9;
-    }
-
-    public void setValueOfSound(int newValueRadio) {
-        if (newValueRadio > 10) {
-            newValueRadio = 10;
-        }
-        if (newValueRadio < 0) {
-            newValueRadio = 0;
-        }
-        valueRadio = newValueRadio;
-
-    }
-
-    public int getValueSound() {
-
-        return valueRadio;
-    }
-
-    public void increaseVolume() {
-        if (valueRadio < 10) {
-            valueRadio += 1;
+        else {
+            System.out.println("Проверь еще раз");
         }
     }
 
-    public void decreaseVolume() {
-        if (valueRadio > 0) {
-            valueRadio -= 1;
-        }
-    }
+
+
 
 }
